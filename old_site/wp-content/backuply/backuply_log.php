e7351e1f0ce5b3198bb3ed2f294f0798eec76aaa
Evans Osumba <osumbaevanzz@gmail.com>
2026-02-20 10:21:12 +0300

    - updated icon list

--- old_site/wp-content/backuply/backuply_log.php
+++
@@ -1,6 +0,0 @@
-<?php exit();?>
-Initializing...|info|13
-<?php exit();?>
-Creating a job to start Backup|info|17
-Stopping the Backup|info|-1
-Stopping the Backup|info|-1
--- old_site/wp-content/plugins-disabled/ovation-elements/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => 'a2551d9641bf3669df8a');
